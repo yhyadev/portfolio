fa23c51e0bbfc5c899d1292a5ab1c722eb89203d
YhyaGithub <yhyamobile57@gmail.com>
2023-07-24 07:30:27 +0300

    feat(components): update hero component

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,13 +1,10 @@
 <div class="hero h-screen text-center">
   <div>
     <div class="name">
-      <h1 class="text-4xl md:text-5xl lg:text-6xl font-extrabold">
-        <span class="text-primary">Yhya</span>
-        <span class="text-accent">Ibrahim</span>
-      </h1>
+      <h1 class="text-4xl md:text-5xl lg:text-6xl font-extrabold">Yhya Ibrahim</h1>
     </div>
     <div class="job">
-      <h2 class="text-lg md:text-xl lg:text-2xl font-bold">Software Developer</h2>
+      <h2 class="text-lg md:text-xl lg:text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary to-accent">Software Developer</h2>
     </div>
   </div>
 </div>
