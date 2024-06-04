9ede7594b4a1ed2a3330a4d3a39986c690145ba3
Yhya <yhyamobile57@gmail.com>
2024-06-04 21:36:45 +0300

    make a layout and add tailwind integration with the theme i want

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        colors: {
+            "black-900": "#232323",
+            "black-800": "#343434",
+            "black-700": "#454545",
+            "black-600": "#565656",
+            "black-500": "#676767",
+            "black-400": "#898989",
+            "black-300": "#9a9a9a",
+            "black-200": "#ababab",
+            "black-100": "#bdbdbd",
+            "white": "#eeeeee",
+            "red": "#cf0a0a",
+        },
+    },
+    plugins: [],
+};
